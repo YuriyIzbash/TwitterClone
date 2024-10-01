43d2ec8d376b1e8eebafd5dcba8eda8627fb7fd8
YuriyIzbash <izbash91@gmail.com>
2024-10-01 12:35:24 +0200

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by YURIY IZBASH on 1. 10. 24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
