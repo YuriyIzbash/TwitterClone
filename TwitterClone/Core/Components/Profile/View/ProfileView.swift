4c83c9e79d570daeea173ba7497531607a1c0be6
YuriyIzbash <izbash91@gmail.com>
2024-10-01 14:50:16 +0200

    Added User Profile UI

---
+++ TwitterClone/Core/Components/Profile/View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  TwitterClone
+//
+//  Created by YURIY IZBASH on 1. 10. 24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
