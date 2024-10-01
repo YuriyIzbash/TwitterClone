e6755d4770e46efc6720c5e783fe1aad5edfdb92
YuriyIzbash <izbash91@gmail.com>
2024-10-01 13:06:36 +0200

    Added Feed UI

--- TwitterClone/Core/Components/Tweets/TweetRowView.swift
+++ TwitterClone/Core/Components/Tweets/TweetRowView.swift
@@ -9,7 +9,75 @@ import SwiftUI
 
 struct TweetRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            // profile image + user info + tweet
+            HStack(alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(.blue)
+                
+                // user info and tweeet caption
+                VStack(alignment: .leading, spacing: 4) {
+                    // user info
+                    HStack {
+                        Text("User Name")
+                            .font(.subheadline).bold()
+                        Text("@user_name")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        Text("2w")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    
+                    //tweet caption
+                    Text("I write something here..")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            // action buttons
+            
+            HStack {
+                Button {
+                    //action goes here
+                } label: {
+                    Image(systemName: "bubble.left")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action goes here
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action goes here
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //action goes here
+                } label: {
+                    Image(systemName: "bookmark")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Divider()
+        }
     }
 }
 
