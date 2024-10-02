43e8d185fc59560cfc1a9c552c8337d607d78981
YuriyIzbash <izbash91@gmail.com>
2024-10-02 23:29:18 +0200

    Added New Tweet UI

---
+++ TwitterClone/Core/Components/TextArea.swift
@@ -0,0 +1,39 @@
+//
+//  TextArea.swift
+//  TwitterClone
+//
+//  Created by YURIY IZBASH on 2. 10. 24.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    @Binding var text: String
+    let placeholder: String
+    
+    init(_ placeholder: String, text: Binding<String>) {
+        self.placeholder = placeholder
+        self._text = text
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color(.placeholderText))
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 12)
+            }
+            
+            TextEditor(text: $text)
+                .padding(4)
+        }
+        .font(.body)
+    }
+}
+
+#Preview {
+    @Previewable @State var text: String = ""
+    TextArea("what's on your mind?", text: $text)
+}
---
+++ TwitterClone/Core/UploadTweet/View/NewTweetView.swift
@@ -0,0 +1,52 @@
+//
+//  NewTweetView.swift
+//  TwitterClone
+//
+//  Created by YURIY IZBASH on 2. 10. 24.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    @State private var caption = ""
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(.systemBlue))
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("Tweet")
+                } label: {
+                    Text("Tweet")
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+            }
+            .padding()
+            
+            HStack(alignment: .top) {
+                Circle()
+                    .frame(width: 64, height: 64)
+                
+                TextArea("What's happening?", text: $caption)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    NewTweetView()
+}
