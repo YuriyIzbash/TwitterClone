7ae917ef0ab81be9caf91f6bab6f3a45e4e2efcb
YuriyIzbash <izbash91@gmail.com>
2024-10-11 12:49:57 +0200

    Added Login UI

---
+++ TwitterClone/Core/Auth/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  TwitterClone
+//
+//  Created by YURIY IZBASH on 11. 10. 24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
---
+++ TwitterClone/Core/Components/RoundedShape.swift
@@ -0,0 +1,8 @@
+//
+//  RoundedShape.swift
+//  TwitterClone
+//
+//  Created by YURIY IZBASH on 11. 10. 24.
+//
+
+import Foundation
