1567fd9ba772963c678a497c58ddefedce5acfef
YuriyIzbash <izbash91@gmail.com>
2024-10-15 10:22:41 +0200

    Added Search and Filter Bar in Explore

--- TwitterClone/Core/Explore/ViewModel/ExploreViewModel.swift
+++ TwitterClone/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -9,6 +9,18 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText: String = ""
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            return users.filter({
+                $0.username.contains(lowercasedQuery) ||
+                $0.fullname.lowercased().contains(lowercasedQuery)
+            })
+        }
+    }
     let service = UserService()
     
     init() {
